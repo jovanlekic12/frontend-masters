c789ac8af54411d8ee782176623c552c5d45eddc
jovanlekic12 <lekicjovan1212@gmail.com>
2025-07-19 23:30:28 +0200

    minor changes

--- src/pages/home/Index.tsx
+++ src/pages/home/Index.tsx
@@ -3,7 +3,7 @@ import SideBar from "./components/sidebar/Index";
 
 export default function Home() {
   return (
-    <main className="max-w-7xl m-20 mx-auto grid grid-cols-[300px_minmax(900px,_1fr)]">
+    <main className="max-w-7xl m-20 mx-auto grid grid-cols-[300px_minmax(900px,_1fr)] md:flex">
       <SideBar />
       <Main />
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,13 +3,14 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    screens: {
-      xl: { max: "1279px" },
-      lg: { max: "1023px" },
-      md: { max: "767px" },
-      sm: { max: "639px" },
+    extend: {
+      screens: {
+        xl: { max: "1279px" },
+        lg: { max: "1023px" },
+        md: { max: "767px" },
+        sm: { max: "639px" },
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
